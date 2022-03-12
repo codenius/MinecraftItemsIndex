2eb293f7526d63b6e45078ed5eabe3332a7bbbc5
codenius <94353572+codenius@users.noreply.github.com>
2022-03-12 18:26:20 +0100

    Restructured and tweaked navigation bar #16

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,31 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-let navToggle = document.querySelector(".nav__toggle");
-let navWrapper = document.querySelector(".nav__wrapper");
+  let hamburger = document.querySelector(".hamburger-icon-container");
+  let navToggle = document.querySelector(".nav__toggle");
+  let navWrapper = document.querySelector(".nav__wrapper");
+  let searchToggle = document.querySelector(".search__toggle");
+  let searchForm = document.querySelector(".search__form");
 
-navToggle.addEventListener("click", function () {
-  if (navWrapper.classList.contains("active")) {
-    this.setAttribute("aria-expanded", "false");
-    this.setAttribute("aria-label", "menu");
-    navWrapper.classList.remove("active");
-  } else {
-    navWrapper.classList.add("active");
-    this.setAttribute("aria-label", "close menu");
-    this.setAttribute("aria-expanded", "true");
+  navToggle.addEventListener("click", () => {
     searchForm.classList.remove("active");
-  }
-});
+    navWrapper.classList.toggle("active");
+  });
 
-let searchToggle = document.querySelector(".search__toggle");
-let searchForm = document.querySelector(".search__form");
-
-searchToggle.addEventListener("click", showSearch);
-
-function showSearch() {
-  searchForm.classList.toggle("active");
-  navToggle.setAttribute("aria-expanded", "false");
-  navToggle.setAttribute("aria-label", "menu");
-  navWrapper.classList.remove("active");
-}
+  searchToggle.addEventListener("click", () => {
+    navWrapper.classList.remove("active");
+    hamburger.classList.remove("hamburger-active");
+    searchForm.classList.toggle("active");
+  })
 
 })
