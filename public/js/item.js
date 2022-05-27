ab697069c10e0b322b7c99b14d94a08f8947a5b6
codenius <94353572+codenius@users.noreply.github.com>
2022-05-27 15:16:12 +0200

    Removed unnecessary code related to bars

--- public/js/item.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-        let array = document.getElementsByClassName("bar");
-        for (let index = 0; index < array.length; index++) {
-            const element = array[index];
-            element.style.width = element.dataset.percentage + "%";
-        }
-    }, 1000);
-});
